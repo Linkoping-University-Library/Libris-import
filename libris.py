83b03ccfe7f8bd6b7e82f570552bd3455fac518c
Håkan Sundblad <hakan.sundblad@gmail.com>
2021-09-03 14:26:14 +0200

    Update libris.py
    Lyft ut anrop till Folio till FolioCommunication.py

--- libris.py
+++ libris.py
@@ -1,114 +1,52 @@
 import requests
 import json
 import os
+import logging
 
-# Läs in FOLIO-settings:
-with open('./settings/folio.ini', 'r') as ini_f:
-    # släng första radens kommentar
-    slask = ini_f.readline()
-    folio_endpoint = ini_f.readline().strip()
-    username = ini_f.readline().strip()
-    password = ini_f.readline().strip()
-    okapi_tenant = ini_f.readline().strip()
-    log_results = ini_f.readline().strip()
+from FolioCommunication import FolioCommunication
 
 
-# Kollar om en fil är tom
-def is_non_zero_file(fpath):  
-    return os.path.isfile(fpath) and os.path.getsize(fpath) > 0
-
-# Logga in och returnera okapiToken
-def getToken(folio_endpoint, username, password, okapi_tenant):
-    login = '/authn/login'
-    url = folio_endpoint + login
-    payload = {"username":username,"password":password}
-    header = {'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'x-okapi-tenant': okapi_tenant}
-
-    response= requests.post(url,data=json.dumps(payload), headers=header)
-    okapiToken = response.headers['x-okapi-token']
-    return okapiToken
+def is_empty_file(fpath):
+    return os.path.isfile(fpath) and os.path.getsize(fpath) == 0
 
 
-########################################################
-okapiToken = getToken(folio_endpoint, username, password, okapi_tenant)
+if __name__ == "__main__":
+#    logging.basicConfig(filename='./log/folio_import.log',
+#                        format='%(levelname)s: %(message)s', level=logging.INFO)
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+    folio = FolioCommunication()
+    libris_dir = './libris_files/'
+    jobprofile = '"jobProfileInfo": {"id": "aa8ff044-2490-4611-ac74-3054aa21e8eb", "name": "Create MARC Bibs", "dataType": "MARC"}'
 
-if log_results == "true":
-    log_f = open('./log/folio_import.log', 'a+')
+    for filename in os.listdir(libris_dir):
+        if filename.endswith('.mrc'):
+            # fil som inte är tom?
+            if not is_empty_file(libris_dir + filename):
+                logging.info('Processing file: %s', filename)
 
-for filename in os.listdir("./libris_files/"):
-    if filename.endswith(".mrc"):
-        # fil som inte är tom?
-        if is_non_zero_file("./libris_files/" + filename):
-            if log_results == "true":
-                log_f.write(filename + "\n")
+                # steg 1
+                stage1_json = folio.uploadDefinitions(filename)
+                uploadDefinitionId = stage1_json['id']
+                fileDefinitionId = stage1_json['fileDefinitions'][0]['id']
 
+                # Steg 2 - skicka in filen
+                with open(libris_dir + filename, 'rb') as f:
+                    data = f.read()
 
+                stage2_json = folio.uploadFile(uploadDefinitionId, fileDefinitionId, data)
 
-        # steg 1
-        url = folio_endpoint + "/data-import/uploadDefinitions"
+                # Steg 3
+                stage2_string = json.dumps(stage2_json)
+                stage2_string = stage2_string.replace("'", '"')
 
-        header = {'Content-Type': 'application/json',
-                 'x-okapi-tenant': okapi_tenant,
-                 'X-Okapi-Token': okapiToken}
+                data = '{"uploadDefinition":' + stage2_string + ', ' + jobprofile + '}'
+                data_json = json.loads(data)
 
-        payload = {"fileDefinitions":[{"name": filename}]}
-        response= requests.post(url,data=json.dumps(payload), headers=header)
+                stage3Response = folio.processFile(uploadDefinitionId, data_json)
 
-        stage1_json = response.json()
+                logging.info('Response from /data-import/uploadDefinitions/{uploadDefinitionId}/processFiles: %s', str(stage3Response.status_code))
+                logging.info('Raw response from /data-import/uploadDefinitions/{uploadDefinitionId}/processFiles: %s', str(stage3Response.raw))
 
-        uploadDefinitionId = stage1_json['id']
-        fileDefinitionId = stage1_json['fileDefinitions'][0]['id']
-
-
-        # Steg 2 - skicka in filen
-        url = folio_endpoint + "/data-import/uploadDefinitions/" + uploadDefinitionId + "/files/" + fileDefinitionId
-
-        header = {'Content-Type': 'application/octet-stream',
-                'x-okapi-tenant': okapi_tenant,
-                'X-Okapi-Token': okapiToken}
-
-        with open("./libris_files/" + filename, 'rb') as f:
-            data = f.read()
-
-        sendResponse = requests.post(url,
-                            data=data,
-                            headers=header)
-
-        stage2_json = sendResponse.json()
-
-        # Steg 3
-
-        url = folio_endpoint + "/data-import/uploadDefinitions/" + uploadDefinitionId + "/processFiles?defaultMapping=true"
-        header = {'Content-Type': 'application/json',
-                'x-okapi-tenant': okapi_tenant,
-                'X-Okapi-Token': okapiToken}
-
-
-        stage2_string = json.dumps(stage2_json)
-        stage2_string = stage2_string.replace("'",'"')
-
-        data = '{"uploadDefinition":' + stage2_string + ', "jobProfileInfo": {"id": "22fafcc3-f582-493d-88b0-3c538480cd83", "name": "Create MARC Bibs", "dataType": "MARC"}}'
-        data_json = json.loads(data)
-
-
-
-        steg3Response = requests.post(url,data=json.dumps(data_json), headers=header)
-
-        if log_results == "true":
-            log_f.write(str(steg3Response.status_code) + "\n")
-            log_f.write(str(steg3Response.raw) + "\n")
-
-
-
-        # Radera filen om allt gick bra
-        os.remove("./libris_files/" + filename)
-
-    else:
-        # radera den tomma filen
-        os.remove("./libris_files/" + filename)
-
-if log_results == "true":
-    log_f.close()
+            # Radera filen (både om den är tom eller om vi bearbetat den)
+            # os.remove(libris_dir + filename)
 
