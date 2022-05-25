26e571affaee85a10e7cdb5ecb6c61e91a6e268b
Linköping University Library <89455263+Linkoping-University-Library@users.noreply.github.com>
2022-05-25 09:30:57 +0200

    Update libris.py

--- libris.py
+++ libris.py
@@ -1,52 +1,96 @@
-import requests
 import json
 import os
+import sys
 import logging
+import pymarc
 
+logging.basicConfig(filename='/usr/local/bin/python_scripts/logs/libris_import.log', filemode = 'a', level=logging.ERROR)
+
+
+sys.path.append('../foliocommunication')
 from FolioCommunication import FolioCommunication
 
-
-def is_empty_file(fpath):
-    return os.path.isfile(fpath) and os.path.getsize(fpath) == 0
+folio = FolioCommunication()
 
 
-if __name__ == "__main__":
-#    logging.basicConfig(filename='./log/folio_import.log',
-#                        format='%(levelname)s: %(message)s', level=logging.INFO)
-    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
-    folio = FolioCommunication()
-    libris_dir = './libris_files/'
-    jobprofile = '"jobProfileInfo": {"id": "aa8ff044-2490-4611-ac74-3054aa21e8eb", "name": "Create MARC Bibs", "dataType": "MARC"}'
+# Kollar om en fil är tom
+def is_non_zero_file(fpath):  
+    return os.path.isfile(fpath) and os.path.getsize(fpath) > 0
 
-    for filename in os.listdir(libris_dir):
-        if filename.endswith('.mrc'):
-            # fil som inte är tom?
-            if not is_empty_file(libris_dir + filename):
-                logging.info('Processing file: %s', filename)
 
-                # steg 1
-                stage1_json = folio.uploadDefinitions(filename)
-                uploadDefinitionId = stage1_json['id']
-                fileDefinitionId = stage1_json['fileDefinitions'][0]['id']
+chunkSize = 200
 
-                # Steg 2 - skicka in filen
-                with open(libris_dir + filename, 'rb') as f:
-                    data = f.read()
+filesFolder = "./libris_files/"
+chunksFolder = "./libris_files/chunks/"
 
-                stage2_json = folio.uploadFile(uploadDefinitionId, fileDefinitionId, data)
+for filename in os.listdir(filesFolder):
+    if filename.endswith(".mrc"):
+        # fil som inte är tom?
+        if is_non_zero_file(filesFolder + filename):
+            
+            with open(filesFolder + filename, 'rb') as data:
+                reader = pymarc.MARCReader(data)
 
-                # Steg 3
-                stage2_string = json.dumps(stage2_json)
-                stage2_string = stage2_string.replace("'", '"')
+                chunkNumber = 0
+                recordCounter = 1
+                outfile = open(chunksFolder + filename.replace(".mrc", "_" + str(chunkNumber) + ".mrc"), "wb")
 
-                data = '{"uploadDefinition":' + stage2_string + ', ' + jobprofile + '}'
-                data_json = json.loads(data)
+                for record in reader:
+                    outfile.write(record.as_marc())
+                    recordCounter += 1
 
-                stage3Response = folio.processFile(uploadDefinitionId, data_json)
+                    if recordCounter > chunkSize:
+                        outfile.flush()
+                        outfile.close
+                        chunkNumber +=1
+                        outfile = open(chunksFolder + filename.replace(".mrc", "_" + str(chunkNumber) + ".mrc"), "wb")
+                        recordCounter = 1
 
-                logging.info('Response from /data-import/uploadDefinitions/{uploadDefinitionId}/processFiles: %s', str(stage3Response.status_code))
-                logging.info('Raw response from /data-import/uploadDefinitions/{uploadDefinitionId}/processFiles: %s', str(stage3Response.raw))
+                outfile.flush()
+                outfile.close
+            
+            os.remove(filesFolder + filename)
 
-            # Radera filen (både om den är tom eller om vi bearbetat den)
-            # os.remove(libris_dir + filename)
+        else:
+            os.remove(filesFolder + filename)
 
+
+
+
+for filename in os.listdir(chunksFolder):
+
+            response = folio.uploadDefinitions(filename)
+            stage1_json = response
+
+
+            uploadDefinitionId = stage1_json['id']
+            fileDefinitionId = stage1_json['fileDefinitions'][0]['id']
+
+
+
+            # Steg 2 - skicka in filen
+            with open(chunksFolder + filename, 'rb') as f:
+                data = f.read()
+
+            sendResponse = folio.uploadFile(uploadDefinitionId, fileDefinitionId, data)
+            stage2_json = sendResponse
+
+
+
+            # Steg 3
+            stage2_string = json.dumps(stage2_json)
+            stage2_string = stage2_string.replace("'",'"')
+
+            data = '{"uploadDefinition":' + stage2_string + ', "jobProfileInfo": {"id": "aa8ff044-2490-4611-ac74-3054aa21e8eb", "name": "Libris", "dataType": "MARC"}}'
+            data_json = json.loads(data)
+
+
+            steg3Response = folio.processFile(uploadDefinitionId, data_json)
+            logging.info(steg3Response)
+           
+
+            # Radera filen om allt gick bra
+            os.remove(chunksFolder + filename)
+            
+
+        
